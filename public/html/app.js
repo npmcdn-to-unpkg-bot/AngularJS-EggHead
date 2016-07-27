c75a5d84732c2c0adc67b57109c6b2f96e2101f8
DzianisL <gestaltldi@yandex.ru>
2016-07-27 13:06:22 +0300

    providers

--- public/html/app.js
+++ public/html/app.js
@@ -1,48 +1,26 @@
-
 var myApp = angular.module("app",["ngRoute"]);
 
-myApp.config(function($routeProvider){
-    $routeProvider.when('/',{
-        templateUrl:"app.html",
-        controller:"viewCtrl",
-        resolve:{
-           loadData:myViewCtrl.loadData
-        }
-    });
-
-});
-
-myApp.directive("error",function($rootScope){
+myApp.provider("game",function(){
+    var type;
     return {
-        restrict:"E",
-        template:"<div ng-show='isError' style='position: relative;color: white;background-color:red;text-align: center;width: 100%;height: 25px;font-weight: bold;font-size: 20px;border-radius: 5px;'>Error</div>",
-        link:function(scope){
-
-            $rootScope.$on("$routeChangeError",function(ev,cur,prev,rej){
-                scope.isError = true;
-            });
-
+        setType:function(value){
+            type = value;
+        },
+        $get:function(){
+            return {
+                title:type + "Game"
+            }
         }
     }
 });
-myApp.controller("appCtrl",function($rootScope){
+
+myApp.config(function(gameProvider){
+    
+    gameProvider.setType("He he he");
+
+});
+myApp.controller("appCtrl",function($scope,game){
   
-   
+   $scope.game = game.title;
+
 });
-
-
-var myViewCtrl = myApp.controller("viewCtrl",function($scope){
-    $scope.model = {
-        message: "Hi!"
-    }
-});
-
-myViewCtrl.loadData = function($q,$timeout){
-
-    var defer = $q.defer();
-    $timeout(function(){
-        defer.reject("Failed to load data");
-    },2000);
-    return defer.promise;
-
-}
