93251a20b7abb6612afe1869e6eff7772fcc80bc
DzianisL <gestaltldi@yandex.ru>
2016-07-27 12:58:29 +0300

    error

--- public/html/app.js
+++ public/html/app.js
@@ -11,12 +11,22 @@ myApp.config(function($routeProvider){
     });
 
 });
+
+myApp.directive("error",function($rootScope){
+    return {
+        restrict:"E",
+        template:"<div ng-show='isError' style='position: relative;color: white;background-color:red;text-align: center;width: 100%;height: 25px;font-weight: bold;font-size: 20px;border-radius: 5px;'>Error</div>",
+        link:function(scope){
+
+            $rootScope.$on("$routeChangeError",function(ev,cur,prev,rej){
+                scope.isError = true;
+            });
+
+        }
+    }
+});
 myApp.controller("appCtrl",function($rootScope){
-
-    $rootScope.$on('$routeChangeError',function(event,current,previous,rejection){
-        console.log(rejection);
-    });
-
+  
    
 });
 
